56a90d4a1a57d0bf55312bf51276d34913b10128
Hubert <kiszkahub@gmail.com>
2024-05-08 19:55:26 +0200

    working

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM openjdk:17-jdk-alpine
 ARG JAR_FILE=target/*.jar
 COPY ./target/pdf-filler-0.0.1-SNAPSHOT.jar app.jar
 COPY ./src/main/resources/main_pdf.pdf /src/main/resources/main_pdf.pdf
+COPY ./src/main/resources/cambria-bold.ttf /src/main/resources/cambria-bold.ttf
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/kiszka/pdffiller/MainController.java
+++ src/main/java/com/kiszka/pdffiller/MainController.java
@@ -71,8 +71,9 @@ public class MainController {
         PDPageContentStream contentStream = new PDPageContentStream(document,page, PDPageContentStream.AppendMode.APPEND,true);
         contentStream.beginText();
         contentStream.setFont(font,9);
-        String text = values[0].replace("\uFEFF", "");
+
         /* FIRST TABLE FILLING */
+        String text = values[0].replace("\uFEFF", "");
         contentStream.newLineAtOffset(138,338);
         contentStream.showText(centerTextSmall(text));
         int idx = 0;
@@ -98,7 +99,55 @@ public class MainController {
         }
         contentStream.setFont(font,14);
         contentStream.newLineAtOffset(OFFSET_X-365,225);
-        contentStream.showText(centerTextBig("jack"));
+        contentStream.showText(values[80].replace("\uFEFF",""));
+        for (int i = 1; i < 9; i++) {
+            contentStream.newLineAtOffset(0,-30);
+            contentStream.showText(centerTextBig(values[80+i].replace("\uFEFF","")));
+        }
+        contentStream.setFont(font,9);
+        contentStream.newLineAtOffset(OFFSET_X+142,-15);
+        contentStream.showText(centerTextSmall(values[89].replace("\uFEFF","")));
+        contentStream.newLineAtOffset(2*OFFSET_X,0);
+        contentStream.showText(centerTextSmall(values[90].replace("\uFEFF","")));
+        /* SECOND TABLE FILLING */
+        text = values[91].replace("\uFEFF", "");
+        contentStream.newLineAtOffset(OFFSET_X+253,241);
+        contentStream.showText(centerTextSmall(text));
+        idx = 0;
+        int offset = 91;
+        for (int i = 92; i < 171; i++) {
+            text = values[i].replace("\uFEFF","");
+            if((i-offset) % 10 == 0) {
+                contentStream.newLineAtOffset(OFFSET_X-275, -OFFSET_Y);
+                idx++;
+            } else {
+                if((i-offset-idx*10)%6==0) {
+                    contentStream.newLineAtOffset(OFFSET_X+3,0);
+                } else if((i-offset-idx*10)%7==0) {
+                    contentStream.newLineAtOffset(OFFSET_X+6,0);
+                } else if((i-offset-idx*10)%8==0){
+                    contentStream.newLineAtOffset(OFFSET_X+5,0);
+                }else if((i-offset-idx*10)%9==0){
+                    contentStream.newLineAtOffset(OFFSET_X+1,0);
+                } else {
+                    contentStream.newLineAtOffset(OFFSET_X,0);
+                }
+            }
+            contentStream.showText(centerTextSmall(text));
+        }
+        contentStream.setFont(font,14);
+        contentStream.newLineAtOffset(OFFSET_X-365,225);
+        contentStream.showText(values[171].replace("\uFEFF",""));
+        for (int i = 1; i < 9; i++) {
+            contentStream.newLineAtOffset(0,-30);
+            contentStream.showText(centerTextBig(values[171+i].replace("\uFEFF","")));
+        }
+        contentStream.setFont(font,9);
+        contentStream.newLineAtOffset(OFFSET_X+142,-15);
+        contentStream.showText(centerTextSmall(values[180].replace("\uFEFF","")));
+        contentStream.newLineAtOffset(2*OFFSET_X,0);
+        contentStream.showText(centerTextSmall(values[181].replace("\uFEFF","")));
+
         contentStream.endText();
         contentStream.close();
         ByteArrayOutputStream output = new ByteArrayOutputStream();
@@ -120,8 +169,7 @@ public class MainController {
     private String centerTextBig(String text) {
         int length = text.length();
         return switch (length) {
-            case 1 -> String.format("%5s",text);
-            case 2,3 -> String.format("%5s",text);
+            case 1,2,3 -> String.format("%5s",text);
             default -> text;
         };
     }
